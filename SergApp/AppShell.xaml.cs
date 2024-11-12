2b12e5aa139574e78c2875a06a8e896aee9d1427
serhiistadnyk <serhii.stadnyk@thescimus.com>
2024-11-12 23:51:48 +0200

    Init commit

---
+++ SergApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SergApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SergApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SergApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
